135fc8fc5eb7c9b38034ea68926598636b64c1be
Enzo Aliatis <enzo.aliatis@gmail.com>
2019-12-12 12:37:24 -0500

    Setting up our react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-enzo"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://pxy2ow5lic.execute-api.us-east-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_jCYxtuGSY",
+    APP_CLIENT_ID: "1c46vkd3107h5lj5dmeve610t5",
+    IDENTITY_POOL_ID: "us-east-1:4e72e72c-f38a-4e9a-9cdd-d4dfa16e1871"
+  }
+}
